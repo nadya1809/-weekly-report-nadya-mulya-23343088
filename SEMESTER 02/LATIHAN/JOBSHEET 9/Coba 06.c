e37790255fa45755c55172b0e1a957f85868e70b
Nadya Mulya <147944008+nadya1809@users.noreply.github.com>
2024-05-22 16:17:03 +0700

    Add files via upload

---
+++ SEMESTER 02/LATIHAN/JOBSHEET 9/Coba 06.c
@@ -0,0 +1,40 @@
+//create by Nadya_23343088 
+
+#include <stdio.h>
+
+void SelSort(int array[], int n);
+
+int main()
+{
+    int array[100], n, i;
+    printf("Masukkan jumlah elemen: ");
+    scanf("%d", &n);
+    printf("Masukkan %d buah bilangan:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &array[i]);
+    SelSort(array, n);
+    return 0;
+}
+
+void SelSort(int array[], int n)
+{
+    int i, j, position, swap;
+    for(i = 0; i < (n - 1); i++)
+    {
+        position = i;
+        for(j = i + 1; j < n; j++)
+        {
+            if(array[position] > array[j])
+                position = j;
+        }
+        if(position != i)
+        {
+            swap = array[i];
+            array[i] = array[position];
+            array[position] = swap;
+        }
+    }
+    printf("Array yang telah diurutkan:\n");
+    for(i = 0; i < n; i++)
+        printf("%d\n", array[i]);
+}
